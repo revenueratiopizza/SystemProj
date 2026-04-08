d736f3a11ddcf1f6fc1a96f473558c8fa2598f46
mredkcm <miller64@sheridancollege.ca>
2026-04-07 21:01:36 -0400

    Zombie now chases Nick anyways

--- Assets/Topdown/ZombieMovement.cs
+++ Assets/Topdown/ZombieMovement.cs
@@ -6,10 +6,14 @@ public class ZombieMovement : MonoBehaviour
     public float speed = 4f;
     public Vector2 movement;
     public GameObject target;
+    public GameObject bulletThatDestroysZombie;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Nick might not always be in the scene
+        // so we need to figure out where he is first
+        target = GameObject.FindGameObjectWithTag("Player");
+        // https://docs.unity3d.com/6000.4/Documentation/Manual/Tags.html
     }
 
     // Update is called once per frame
@@ -17,7 +21,7 @@ public class ZombieMovement : MonoBehaviour
     {
         transform.position += (Vector3)movement * speed * Time.deltaTime;
         // Same thing as PlayerMovement but now it's facing towards the target
-        Vector2 direction = target.transform.position - transform.position;
+        Vector2 direction = target.transform.position - transform.position; 
         // Target has to be the player at all times
         transform.up = direction;
         transform.position += (Vector3)direction.normalized * speed * Time.deltaTime;
@@ -25,5 +29,8 @@ public class ZombieMovement : MonoBehaviour
         //
         // I'm gonna be honest I heard "normalized" in a video once and I thought it sounded really cool. Like that's literally all I'm working off of
         // https://docs.unity3d.com/6000.3/Documentation/ScriptReference/Vector3-normalized.html
+
+        // If this object (the zombie) "collides" with bulletThatDestroysZombie(if their X position is greater than the X position of the bullet - 10 or kess than the X position of the bullet + 10)
+        // Then destroy the zombie
     }
 }
