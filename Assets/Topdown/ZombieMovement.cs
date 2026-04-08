437b26dc924cbf6594f746844cace56d04856c79
mredkcm <miller64@sheridancollege.ca>
2026-04-07 21:48:53 -0400

    Zombies constantly spawn. Bullets track properly

--- Assets/Topdown/ZombieMovement.cs
+++ Assets/Topdown/ZombieMovement.cs
@@ -6,7 +6,7 @@ public class ZombieMovement : MonoBehaviour
     public float speed = 4f;
     public Vector2 movement;
     public GameObject target;
-    public GameObject bulletThatDestroysZombie;
+    public GameObject[] bulletThatDestroysZombie;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,23 +14,40 @@ public class ZombieMovement : MonoBehaviour
         // so we need to figure out where he is first
         target = GameObject.FindGameObjectWithTag("Player");
         // https://docs.unity3d.com/6000.4/Documentation/Manual/Tags.html
+
+        // Let's also do the same with the bullets in Update
+        // For some reason I thought Start and Update were the same thing when writing this code
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += (Vector3)movement * speed * Time.deltaTime;
-        // Same thing as PlayerMovement but now it's facing towards the target
-        Vector2 direction = target.transform.position - transform.position; 
+        // Same thing as PlayerMovement but now it's facing towards the player
+        Vector2 direction = target.transform.position - transform.position;
         // Target has to be the player at all times
         transform.up = direction;
         transform.position += (Vector3)direction.normalized * speed * Time.deltaTime;
-        // I want the normalized vector because
-        //
-        // I'm gonna be honest I heard "normalized" in a video once and I thought it sounded really cool. Like that's literally all I'm working off of
+        // I want the normalized vector because I heard "normalized" in a video once and I thought the term sounded slick
         // https://docs.unity3d.com/6000.3/Documentation/ScriptReference/Vector3-normalized.html
 
-        // If this object (the zombie) "collides" with bulletThatDestroysZombie(if their X position is greater than the X position of the bullet - 10 or kess than the X position of the bullet + 10)
-        // Then destroy the zombie
+        // https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html
+        // Thank you docs.unity3d.com for coming in clutch
+        // I still don't know how to use arrays though
+
+        bulletThatDestroysZombie = GameObject.FindGameObjectsWithTag("Bullet");
+        foreach (GameObject bulletThatDestroysZombie in bulletThatDestroysZombie)
+        {
+            if (bulletThatDestroysZombie != null) // I should add a null here to make sure it doesn't return an error with no bullets
+            {
+                if (Vector2.Distance(transform.position, bulletThatDestroysZombie.transform.position) < 0.5f)
+                    // Atp I'm just throwing stuff at the wall
+                {
+                    Destroy(gameObject);
+                    // No strict collision checks because I don't remember if we learned this in class
+                    // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Distance.html
+                }
+            }
+        }
     }
 }
--- Assets/Topdown/ZombieSpawner.cs
+++ Assets/Topdown/ZombieSpawner.cs
@@ -1,24 +1,37 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 // https://www.youtube.com/watch?v=qrr4BGRcv3I
 // https://www.youtube.com/watch?v=_4TRkQK7nNs
 // https://www.youtube.com/watch?v=7qvuuQ1oCMk
 public class ZombieSpawner : MonoBehaviour
 {
     public GameObject zombiePrefab;
+    IEnumerator ZombieSpawning()
+    {
+        // It seems as though while(true) is an infinite loop
+        // https://www.reddit.com/r/C_Programming/comments/u0ibw7/what_is_the_fastest_and_best_way_to_do_an/
+        // Good to know
+        while (true)
+        {
+            // Remake the random spawn floats every time we run the coroutine just for good measure
+            float zombieSpawnX = Random.Range(-10f, 10f);
+            float zombieSpawnY = Random.Range(-6f, 6f);
+            // And then we use these two values to randomize a zombie's spawn position
+            Vector2 spawnPosition = new Vector2(zombieSpawnX, zombieSpawnY);
+            Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
+            // And we do that every 0.1-1 seconds and loop
+            yield return new WaitForSeconds(Random.Range(0.1f, 1f));
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        StartCoroutine(ZombieSpawning());
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-    }
-
-    public void OnAttack(InputAction.CallbackContext context)
     {
 
     }
